42bd54177cdd044c1042db4a47eaa1d8bff742e2
vysmbln <vysmbln@users.noreply.github.com>
2020-01-28 17:28:41 -0500

    timer-constructor

--- Timer/index.js
+++ Timer/index.js
@@ -1,1 +1,19 @@
-console.log("hello!")
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+this.durationInput = durationInput;
+this.startButton = startButton;
+this.pauseButton = pauseButton;
+
+this.startButton.addEventListener('click', this.start)
+    }
+
+    start() {
+        console.log('timer start');
+    }
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton)
